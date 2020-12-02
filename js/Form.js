1efbc53467c32c4731d932515b1d10763a27bdcf
yamunah96 <70136290+yamunah96@users.noreply.github.com>
2020-12-02 16:50:21 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,53 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+    this.title = createElement('h2');
+    this.reset = createButton('Reset');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display(){
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 50, 0);
+
+    this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 30, displayHeight/2);
+    this.reset.position(displayWidth-100,20);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    });
+
+    // reset the data in database
+
+    this.reset.mousePressed(()=>{
+      player.updateCount(0);
+      game.update(0);
+     
+      // we can add two ways to update the CarsAtEnd 
+      // 1. player. updateCarsAtEnd(0);
+      //2.CarsAtEnd:0;
+      database.ref('/').update({
+        players:null,
+        CarsAtEnd:0
+      })
+    });
+
+  }
+}
